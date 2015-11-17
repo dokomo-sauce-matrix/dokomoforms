fb2e9719d1a4c10a0551d9d9a03a2c0c06843bc9
Viktor Roytman <vr2262@columbia.edu>
2015-11-17 11:50:29 -0500

    WIP

--- tests/python/test_selenium.py
+++ tests/python/test_selenium.py
@@ -3262,34 +3262,14 @@ class TestEnumerate(DriverTest):
             'b0'
         )
         self.click(self.drv.find_element_by_class_name('page_nav__prev'))
-        (
-            ActionChains(self.drv)
-            .key_down(
-                self.control_key,
-                self.drv.find_element_by_tag_name('input')
-            )
-            .send_keys('a')
-            .key_up(self.control_key)
-            .send_keys('15.1')
-            .perform()
-        )
+        self.input_field().send_keys(Keys.BACK_SPACE * 4, '15.1')
         self.click(self.drv.find_element_by_class_name('navigate-right'))
         self.assertEqual(
             self.drv.find_element_by_tag_name('h3').text,
             'b1'
         )
         self.click(self.drv.find_element_by_class_name('page_nav__prev'))
-        (
-            ActionChains(self.drv)
-            .key_down(
-                self.control_key,
-                self.drv.find_element_by_tag_name('input')
-            )
-            .send_keys('a')
-            .key_up(self.control_key)
-            .send_keys('5.1')
-            .perform()
-        )
+        self.input_field().send_keys(Keys.BACK_SPACE * 4, '5.1')
         self.click(self.drv.find_element_by_class_name('navigate-right'))
         self.assertEqual(
             self.drv.find_element_by_tag_name('h3').text,
