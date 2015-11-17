c0c38bdc6f20fefec97228f8f0ef8f4bac8e4c90
Viktor Roytman <vr2262@columbia.edu>
2015-11-17 13:06:18 -0500

    WIP

--- tests/python/test_selenium.py
+++ tests/python/test_selenium.py
@@ -3294,17 +3294,7 @@ class TestEnumerate(DriverTest):
             'b0'
         )
         self.click(self.drv.find_element_by_class_name('page_nav__prev'))
-        (
-            ActionChains(self.drv)
-            .key_down(
-                self.control_key,
-                self.drv.find_element_by_tag_name('input')
-            )
-            .send_keys('a')
-            .key_up(self.control_key)
-            .send_keys('999.1')
-            .perform()
-        )
+        self.input_field().send_keys(Keys.BACK_SPACE * 6, '999.1')
         self.click(self.drv.find_element_by_class_name('navigate-right'))
         self.assertEqual(
             self.drv.find_element_by_tag_name('h3').text,
